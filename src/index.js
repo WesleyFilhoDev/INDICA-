1440bfb4581c8e68095b0384ba323d0896ab1a73
WesleyFilhoDev <wesleyfilho221204@gmail.com>
2025-12-07 14:17:04 -0300

    ambiente configurado

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("API funcionando!");
+});
+
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000");
+});
