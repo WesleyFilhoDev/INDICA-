c6e4988ae23442fcf1505d2c1e00f7ae0c0c30ca
WesleyFilhoDev <wesleyfilho221204@gmail.com>
2025-12-07 23:36:45 -0300

    setando a porta

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 app.use(express.json());
@@ -9,6 +11,8 @@ app.get("/", (req, res) => {
   res.send("API funcionando!");
 });
 
-app.listen(3000, () => {
-  console.log("Servidor rodando na porta 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
