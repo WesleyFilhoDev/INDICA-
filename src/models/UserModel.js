68752e7ed8ade62b6548350adb6a9e2c63388eaa
WesleyFilhoDev <wesleyfilho221204@gmail.com>
2025-12-10 13:26:19 -0300

    refactor: clean up model name comment in UserModel

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -2,7 +2,7 @@ const { DataTypes } = require("sequelize");
 const sequelize = require("../config/database");
 
 const UserModel = sequelize.define(
-  "User", // nome do model
+  "User",
   {
     id: {
       type: DataTypes.UUID,
